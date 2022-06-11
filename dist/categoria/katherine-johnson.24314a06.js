5a4564dfec7cc5f20bda6e81e9e6ae10fd37f5a5
jricolfe <jricolfe@uoc.edu>
2022-06-11 23:10:43 +0200

    pac-3-iteracio-1-prova1

---
+++ dist/categoria/katherine-johnson.24314a06.js
@@ -0,0 +1,2 @@
+function e(e){e.preventDefault(),confirm("¿Estás segurx de que quieres salir de la página?")&&(window.location="https://ca.wikipedia.org/wiki/Hidden_Figures")}window.onload=function(){document.getElementById("salida").addEventListener("click",e,!1)};
+//# sourceMappingURL=katherine-johnson.24314a06.js.map
---
+++ dist/index.354a4574.js
@@ -0,0 +1,2 @@
+function salida(i){i.preventDefault(),confirm("¿Estás segurx de que quieres salir de la página?")&&(window.location="https://ca.wikipedia.org/wiki/Hidden_Figures")}function load(){document.getElementById("salida").addEventListener("click",salida,!1)}window.onload=load;
+//# sourceMappingURL=index.354a4574.js.map
