27a0420d6a848cc392027719d624056113bd045b
jricolfe <jricolfe@uoc.edu>
2022-06-12 18:16:44 +0200

    pac3-iteracio2-prova5

--- dist/categoria/katherine-johnson.2437d737.js
+++
@@ -1,2 +0,0 @@
-function e(e){e.preventDefault(),confirm("¿Estás segurx de que quieres salir de la página?")&&(window.location="https://ca.wikipedia.org/wiki/Hidden_Figures")}window.onload=function(){document.getElementById("salida").addEventListener("click",e,!1)};
-//# sourceMappingURL=katherine-johnson.2437d737.js.map
